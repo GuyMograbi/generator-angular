1ed63da99b1f1e4f3ed3a55e79bf4382d840f5f6
Mukta Garodia <mukta.garodia@gmail.com>
2016-02-13 17:55:26 +0200

    fix test

--- templates/javascript/spec/directive.js
+++ templates/javascript/spec/directive.js
@@ -10,16 +10,16 @@ describe('Directive: <%= cameledName %>', function () {
     $compile,
     scope;
 
-  beforeEach(inject(function ( _$rootScope_ , _$compile_ ) {
+  beforeEach(inject(function (_$rootScope_, _$compile_) {
     $rootScope = _$rootScope_;
     $compile = _$compile_;
     scope = $rootScope.$new();
     element = angular.element('<div <%= _.dasherize(name) %>></div>');
     element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the <%= cameledName %> directive');
+
   }));
 
-  it('should make hidden element visible', function() {
-
+  it('should make hidden element visible', function () {
+    expect(element.text()).toBe('this is the <%= cameledName %> directive');
   });
 });
